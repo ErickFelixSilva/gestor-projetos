ed58828a570b0805e26fcb5558031c409ce862bd
Erick Felix da Silva <erickfelix16@gmail.com>
2024-05-24 16:11:48 -0300

    Commit inicial

---
+++ src/main/java/com/challenge/gestor_projetos/GestorProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.gestor_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/gestor_projetos/GestorProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.gestor_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
